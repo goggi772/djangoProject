bf0e3e2c2d51648809629d050c77e25a850ef005
SunYub <goggi772@gmail.com>
2023-07-24 17:58:16 +0900

    StartBootStrap Initial Setting

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from mysite.models import MainContent
+
+
 # Create your views here.
+def index(request):
+    # return HttpResponse("Hello, World!")
+
+    # content_list = MainContent.objects.order_by('-pub_date')
+    # context = {'content_list': content_list}
+    return render(request, 'mysite/home.html')
+
+
+def about(request):
+    return render(request, 'mysite/about.html')
+
+
+def contact(request):
+    return render(request, 'mysite/contact.html')
+
+
+def post(request):
+    return render(request, 'mysite/post.html')
